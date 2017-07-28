7ad8a94bc0fcf47656db6efa60c837474830e4e4
danielaramos123 <30536951+danielaramos123@users.noreply.github.com>
2017-07-28 15:46:19 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+  var a=document.createElement('a');
+  a.href='http:/instagram.com/daniela_ramos29';
+  var image = document.getElementById('mydiv').getElementsByTagName('img')[0];
+  b=a.appendChild(image);
+  document.getElementById('mydiv').appendChild(a);
